d2fbfeb5ea650e1c601b1d68e6ef32e98aad0c97
sarthakGoNovaData <sarthak@godatanova.com>
2026-02-11 14:53:35 +0530

    Init Commit

---
+++ front-end/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <header>
+        <h1>Welcome to EnableU Colab</h1>
+      </header>
+      <main>
+        <p>Project initialized successfully.</p>
+      </main>
+    </div>
+  )
+}
+
+export default App
